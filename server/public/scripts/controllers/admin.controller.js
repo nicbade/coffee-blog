b53a52e4ee3a872af5e80384bb3c581e5d66961a
Nic Bade <chipchapmaniii@gmail.com>
2017-11-14 10:29:34 -0600

    all reviews on DOM

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,9 +1,10 @@
 myApp.controller('AdminController', function (AdminService, NgMap) {
   // console.log('AdminController created');
-  
+
   var self = this;
   self.AdminService = AdminService;
   self.newReview = AdminService.review;
+  console.log('self.newReview: ', self.newReview)
 
   NgMap.getMap().then(function (map) {
     self.map = map;
@@ -20,11 +21,15 @@ myApp.controller('AdminController', function (AdminService, NgMap) {
     console.log('addReview post route: ', self.newReview)
     AdminService.addReview(self.newReview);
     self.newReview = {};
+    console.log('I\'m watching')
   };
 
   // INITIATES GET ROUTE FOR REVIEWS
   AdminService.getReview();
 
-
+  self.showDetail = function (e, review) {
+    self.newReview.list.name = name;
+    self.map.showInfoWindow('reviewWindow', this);
+  };
 
 });
